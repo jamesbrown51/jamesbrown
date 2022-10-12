36e0df97f1e2f561e0e506039d3531975e18d900
jamesbrown <halepoglu@gmail.com>
2022-10-12 01:53:01 -0400

    for dongusu eklendi

---
+++ src/Merhaba.java
@@ -0,0 +1,9 @@
+public class Merhaba {
+    public static void main(String[] args) {
+        System.out.println("merhaba dunya");
+
+        for (int i =0; i<5; i++){
+            System.out.println("merhaba dunya");
+        }
+    }
+}
