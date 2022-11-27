c69f7708fd4c2ca12b622ebe6c3841ff091681de
jamesbrown51 <115384237+jamesbrown51@users.noreply.github.com>
2022-11-27 12:24:13 -0500

    Update Merhaba.java

--- src/Merhaba.java
+++ src/Merhaba.java
@@ -1,15 +1,15 @@
 public class Merhaba {
     public static void main(String[] args) {
-        System.out.println("merhaba dunya");
+        System.out.println("hello world");
 
         for (int i =0; i<5; i++){
-            System.out.println("cozecem bu isi");
+            System.out.println("must figure out this");
 
-            double sayi=456;
-            double sayi2=122;
+            double num=456;
+            double num2=122;
 
-            double toplam=sayi2/sayi;
-            System.out.println("sonuc = " + toplam);
+            double sum=num2/num;
+            System.out.println("result = " + sum);
 
 
         }
